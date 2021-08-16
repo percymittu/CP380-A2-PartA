84a24599507629fd0d872dcd774a985057b31a88
Percy Mittagadapala <willi.percy3@gmail.com>
2021-08-16 11:12:25 -0400

    Assignment 2 Part A

---
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+
+namespace CP380_B1_BlockList.Models
+{
+    public enum TransactionTypes
+    {
+        BUY, SELL, GRANT
+    }
+
+    public class Payload
+    {
+        // TODO        
+        public string User { get; set; }
+        public int TransactionType { get; set; }
+        public int Amount { get; set; }
+        public string Item { get; set; }
+
+        public Payload(string user, TransactionTypes t, int amount, string item)
+        {
+            User = user;
+            TransactionType = (int)t;
+            Amount = amount;
+            Item = item;
+
+        }
+    }
+}
